837bd66d55044f9ed0b9e9b50d9927072594f24c
Wisemuji <wisemuji@gmail.com>
2021-01-07 00:17:49 +0900

    Use camel case for properies

--- data/src/main/java/com/wisemuji/data/entity/ProfileResponse.kt
+++ data/src/main/java/com/wisemuji/data/entity/ProfileResponse.kt
@@ -10,8 +10,8 @@ data class ProfileResponse(
     @Json(name = "blog") val blog: String? = null,
     @Json(name = "location") val location: String? = null,
     @Json(name = "bio") val bio: String? = null,
-    @Json(name = "public_repos") val public_repos: Int? = null,
-    @Json(name = "public_gists") val public_gists: Int? = null,
+    @Json(name = "public_repos") val publicRepos: Int? = null,
+    @Json(name = "public_gists") val publicGists: Int? = null,
     @Json(name = "followers") val followers: Int? = null,
     @Json(name = "following") val following: Int? = null,
 )
