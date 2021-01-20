a00bfc19bd2439e782a2ff3c0207a88c3f2d3843
Suhyeon Kim <wisemuji@gmail.com>
2021-01-21 03:57:52 +0900

    도메인 레이어 추가 (#4)

---
+++ domain/src/main/java/com/wisemuji/domain/FollowersDto.kt
@@ -0,0 +1,14 @@
+package com.wisemuji.domain
+
+import com.wisemuji.data.entity.FollowersResponse
+
+data class FollowersDto(
+    val username: String? = null,
+) {
+
+    companion object {
+        fun fromEntity(followersResponse: FollowersResponse) = FollowersDto(
+            username = followersResponse.username
+        )
+    }
+}
