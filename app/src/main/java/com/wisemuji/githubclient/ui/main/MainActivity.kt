1ad76b06fd9f47f51783003f3ac1a40ff536ecc7
Wisemuji <wisemuji@gmail.com>
2021-02-02 04:33:47 +0900

    [Add] MainActivity

---
+++ app/src/main/java/com/wisemuji/githubclient/ui/main/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.wisemuji.githubclient.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.wisemuji.githubclient.R
+import com.wisemuji.githubclient.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupBinding()
+        setupView()
+    }
+
+    private fun setupBinding() {
+        binding = DataBindingUtil
+            .setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+            .also {
+                it.lifecycleOwner = this
+                it.viewModel = viewModel
+            }
+    }
+
+    private fun setupView() {
+        viewModel.setupView()
+    }
+}
