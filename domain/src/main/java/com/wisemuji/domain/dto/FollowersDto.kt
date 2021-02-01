bac14b6d88f397e4e3eb264137e87d477e97466e
Wisemuji <wisemuji@gmail.com>
2021-02-02 04:52:03 +0900

    [Review] fix to one line

--- domain/src/main/java/com/wisemuji/domain/dto/FollowersDto.kt
+++ domain/src/main/java/com/wisemuji/domain/dto/FollowersDto.kt
@@ -2,14 +2,10 @@ package com.wisemuji.domain.dto
 
 import com.wisemuji.data.entity.FollowersResponse
 
-data class FollowersDto(
-    val username: String? = null,
-) {
+data class FollowersDto(val username: String? = null) {
 
     companion object {
         fun fromEntity(followersResponse: FollowersResponse) =
-            FollowersDto(
-                username = followersResponse.username
-            )
+            FollowersDto(username = followersResponse.username)
     }
 }
